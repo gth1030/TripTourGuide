aaf0e94fb0e3be4e7ede164cd2ebf52b8f3bc2a1
minicomet <56183155+minicomet@users.noreply.github.com>
2019-10-05 15:15:29 +0900

    commit(2th/10.05)

---
+++ TripTourGuide/app/src/main/java/com/example/triptourguide/after_trip.java
@@ -0,0 +1,14 @@
+package com.example.triptourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class after_trip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_trip);
+    }
+}
---
+++ TripTourGuide/app/src/main/java/com/example/triptourguide/while_trip.java
@@ -0,0 +1,14 @@
+package com.example.triptourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class while_trip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_while_trip);
+    }
+}
