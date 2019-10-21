12a299782ae7db2436b93b54e0da9a8b825400a3
minicomet <56183155+minicomet@users.noreply.github.com>
2019-10-21 23:00:30 +0900

    10.21preparelistsetting

---
+++ TripTourGuide/app/src/main/java/com/example/triptourguide/prepareItem.java
@@ -0,0 +1,14 @@
+package com.example.triptourguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class prepareItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare_item);
+    }
+}
